d1fa318d2e8bad1fc8e6c69a483844b0456a5e15
Swapnil Bhadade <swapnilrbhadade83@gmail.com>
2018-05-16 13:59:28 +0530

    Update mlstartup.sh

--- mlstartup.sh
+++ mlstartup.sh
@@ -6,7 +6,7 @@ source bin/activate
 # Create A ml ENV 
 conda create -n ml pip python=3.6
 cd envs/ml
-source activate 
+source activate ml
 
 #pytorch python 3.6 with cuda 8.0
 conda install pytorch torchvision -c pytorch
@@ -15,4 +15,7 @@ conda install pytorch torchvision -c pytorch
 pip install --ignore-installed --upgrade \https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.8.0-cp36-cp36m-linux_x86_64.whl
 
 # Scipy, Numpy etc. 
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+conda install -c anaconda scipy
+conda install -c anaconda numpy
+conda install -c conda-forge matplotlib
+conda install -c conda-forge/label/rc matplotlibytlib ken matplotlib
